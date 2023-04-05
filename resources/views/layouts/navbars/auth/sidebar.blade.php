06d20e5680bd425563ef0a94647adf5e9cbd684f
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-04-06 01:15:51 +0600

    updated

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -34,6 +34,168 @@
         <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Laravel Examples</h6>
       </li>
       <li class="nav-item">
+        <a data-bs-toggle="collapse" href="#componentsExamples" class="nav-link " aria-controls="componentsExamples"
+            role="button" aria-expanded="false">
+            <div
+                class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center d-flex align-items-center justify-content-center  me-2">
+                <svg width="12px" height="12px" viewBox="0 0 46 42" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <title>customer-support</title>
+                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <g transform="translate(-1717.000000, -291.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                            <g transform="translate(1716.000000, 291.000000)">
+                                <g transform="translate(1.000000, 0.000000)">
+                                    <path class="color-background"
+                                        d="M45,0 L26,0 C25.447,0 25,0.447 25,1 L25,20 C25,20.379 25.214,20.725 25.553,20.895 C25.694,20.965 25.848,21 26,21 C26.212,21 26.424,20.933 26.6,20.8 L34.333,15 L45,15 C45.553,15 46,14.553 46,14 L46,1 C46,0.447 45.553,0 45,0 Z"
+                                        opacity="0.59858631"></path>
+                                    <path class="color-foreground"
+                                        d="M22.883,32.86 C20.761,32.012 17.324,31 13,31 C8.676,31 5.239,32.012 3.116,32.86 C1.224,33.619 0,35.438 0,37.494 L0,41 C0,41.553 0.447,42 1,42 L25,42 C25.553,42 26,41.553 26,41 L26,37.494 C26,35.438 24.776,33.619 22.883,32.86 Z">
+                                    </path>
+                                    <path class="color-foreground"
+                                        d="M13,28 C17.432,28 21,22.529 21,18 C21,13.589 17.411,10 13,10 C8.589,10 5,13.589 5,18 C5,22.529 8.568,28 13,28 Z">
+                                    </path>
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                </svg>
+            </div>
+            <span class="nav-link-text ms-1">Components</span>
+        </a>
+        <div class="collapse " id="componentsExamples">
+            <ul class="nav ms-4 ps-3">
+                <li class="nav-item ">
+                    <a class="nav-link " href="https://www.creative-tim.com/learning-lab/bootstrap/alerts/soft-ui-dashboard"
+                        target="_blank">
+                        <span class="sidenav-mini-icon"> A </span>
+                        <span class="sidenav-normal"> Alerts </span>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link " href="https://www.creative-tim.com/learning-lab/bootstrap/badge/soft-ui-dashboard"
+                        target="_blank">
+                        <span class="sidenav-mini-icon"> B </span>
+                        <span class="sidenav-normal"> Badge </span>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link "
+                        href="https://www.creative-tim.com/learning-lab/bootstrap/buttons/soft-ui-dashboard"
+                        target="_blank">
+                        <span class="sidenav-mini-icon"> B </span>
+                        <span class="sidenav-normal"> Buttons </span>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link " href="https://www.creative-tim.com/learning-lab/bootstrap/cards/soft-ui-dashboard"
+                        target="_blank">
+                        <span class="sidenav-mini-icon"> C </span>
+                        <span class="sidenav-normal"> Card </span>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link "
+                        href="https://www.creative-tim.com/learning-lab/bootstrap/carousel/soft-ui-dashboard"
+                        target="_blank">
+                        <span class="sidenav-mini-icon"> C </span>
+                        <span class="sidenav-normal"> Carousel </span>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link "
+                        href="https://www.creative-tim.com/learning-lab/bootstrap/collapse/soft-ui-dashboard"
+                        target="_blank">
+                        <span class="sidenav-mini-icon"> C </span>
+                        <span class="sidenav-normal"> Collapse </span>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link "
+                        href="https://www.creative-tim.com/learning-lab/bootstrap/dropdowns/soft-ui-dashboard"
+                        target="_blank">
+                        <span class="sidenav-mini-icon"> D </span>
+                        <span class="sidenav-normal"> Dropdowns </span>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link " href="https://www.creative-tim.com/learning-lab/bootstrap/forms/soft-ui-dashboard"
+                        target="_blank">
+                        <span class="sidenav-mini-icon"> F </span>
+                        <span class="sidenav-normal"> Forms </span>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link " href="https://www.creative-tim.com/learning-lab/bootstrap/modal/soft-ui-dashboard"
+                        target="_blank">
+                        <span class="sidenav-mini-icon"> M </span>
+                        <span class="sidenav-normal"> Modal </span>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link " href="https://www.creative-tim.com/learning-lab/bootstrap/navs/soft-ui-dashboard"
+                        target="_blank">
+                        <span class="sidenav-mini-icon"> N </span>
+                        <span class="sidenav-normal"> Navs </span>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link " href="https://www.creative-tim.com/learning-lab/bootstrap/navbar/soft-ui-dashboard"
+                        target="_blank">
+                        <span class="sidenav-mini-icon"> N </span>
+                        <span class="sidenav-normal"> Navbar </span>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link "
+                        href="https://www.creative-tim.com/learning-lab/bootstrap/pagination/soft-ui-dashboard"
+                        target="_blank">
+                        <span class="sidenav-mini-icon"> P </span>
+                        <span class="sidenav-normal"> Pagination </span>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link "
+                        href="https://www.creative-tim.com/learning-lab/bootstrap/popovers/soft-ui-dashboard"
+                        target="_blank">
+                        <span class="sidenav-mini-icon"> P </span>
+                        <span class="sidenav-normal"> Popovers </span>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link "
+                        href="https://www.creative-tim.com/learning-lab/bootstrap/progress/soft-ui-dashboard"
+                        target="_blank">
+                        <span class="sidenav-mini-icon"> P </span>
+                        <span class="sidenav-normal"> Progress </span>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link "
+                        href="https://www.creative-tim.com/learning-lab/bootstrap/spinners/soft-ui-dashboard"
+                        target="_blank">
+                        <span class="sidenav-mini-icon"> S </span>
+                        <span class="sidenav-normal"> Spinners </span>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link " href="https://www.creative-tim.com/learning-lab/bootstrap/tables/soft-ui-dashboard"
+                        target="_blank">
+                        <span class="sidenav-mini-icon"> T </span>
+                        <span class="sidenav-normal"> Tables </span>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link "
+                        href="https://www.creative-tim.com/learning-lab/bootstrap/tooltips/soft-ui-dashboard"
+                        target="_blank">
+                        <span class="sidenav-mini-icon"> T </span>
+                        <span class="sidenav-normal"> Tooltips </span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </li>
+      <li class="nav-item">
         <a class="nav-link {{ (Request::is('user-profile') ? 'active' : '') }} " href="{{ url('user-profile') }}">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                 <svg width="12px" height="12px" viewBox="0 0 46 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
