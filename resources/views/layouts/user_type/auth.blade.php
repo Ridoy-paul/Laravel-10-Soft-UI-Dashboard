b6c7fe6606179cd87843e566b77f0ffdac12157c
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-04-06 01:22:02 +0600

    components fixed plugin updated

--- resources/views/layouts/user_type/auth.blade.php
+++ resources/views/layouts/user_type/auth.blade.php
@@ -2,7 +2,6 @@
 
 @section('auth')
 
-
     @if(\Request::is('static-sign-up')) 
         @include('layouts.navbars.guest.nav')
         @yield('content')
@@ -15,15 +14,14 @@
     
     @else
         @if (\Request::is('rtl'))  
-            @include('layouts.navbars.auth.sidebar-rtl')
+            {{-- @include('layouts.navbars.auth.sidebar-rtl')
             <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg overflow-hidden">
                 @include('layouts.navbars.auth.nav-rtl')
                 <div class="container-fluid py-4">
                     @yield('content')
                     @include('layouts.footers.auth.footer')
                 </div>
-            </main>
-
+            </main> --}}
         @elseif (\Request::is('profile'))  
             @include('layouts.navbars.auth.sidebar')
             <div class="main-content position-relative bg-gray-100 max-height-vh-100 h-100">
@@ -40,7 +38,6 @@
                 </main>
             </div>
             @include('layouts.footers.auth.footer')
-
         @else
             @include('layouts.navbars.auth.sidebar')
             <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg {{ (Request::is('rtl') ? 'overflow-hidden' : '') }}">
@@ -52,7 +49,7 @@
             </main>
         @endif
 
-        @include('components.fixed-plugin')
+        {{-- @include('components.fixed-plugin') --}}
     @endif
 
     
